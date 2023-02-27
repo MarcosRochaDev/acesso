6a9d031ce8f1b1d8471cfd49b15dce60628ee700
MarcosRochaDev <108995770+MarcosRochaDev@users.noreply.github.com>
2023-02-27 09:44:28 -0300

    Up

--- html/custom.js
+++ html/custom.js
@@ -1,24 +1,28 @@
-let time = 6000,
+let time = 3000,
     Indexdiv = 0,
     divs = document.querySelectorAll('.carrocel div'),
     max = divs.length;
-    /* console.log(divs + max) */
+    console.log(max)
 
     function nextdiv(){
         console.log(Indexdiv)
-        divs[Indexdiv].classList.remove('container-carrocel');
-        divs[Indexdiv].classList.add('hidecarrocel');
+
+
+        divs[Indexdiv].classList.remove('hidecarrocel');
+        divs[Indexdiv].classList.add('container-carrocel');
         
         Indexdiv++;
 
         
         if(Indexdiv >= max) {
             Indexdiv = 0;}
-            divs[Indexdiv].classList.remove('hidecarrocel');
-            divs[Indexdiv].classList.add('container-carrocel');
-        
+            divs[Indexdiv].classList.remove('container-carrocel');
+            divs[Indexdiv].classList.add('hidecarrocel');
+  
+            
         
     }
+    
 
 
 
